f24e43920e90dedbd0d7837fc59396046d9cc070
qiwang <timer_sevenwang@163.com>
2017-12-27 16:35:12 +0800

    create

---
+++ SWFoundation/SWFoundation/SWFoundation.h
@@ -0,0 +1,19 @@
+//
+//  SWFoundation.h
+//  SWFoundation
+//
+//  Created by seven on 2017/12/27.
+//  Copyright © 2017年 seven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SWFoundation.
+FOUNDATION_EXPORT double SWFoundationVersionNumber;
+
+//! Project version string for SWFoundation.
+FOUNDATION_EXPORT const unsigned char SWFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SWFoundation/PublicHeader.h>
+
+
